75d8b0117d8627ec6c3fcb4266f2a277028a3966
Andrija Petrovic <andrija.hers@gmail.com>
2017-06-30 06:59:47 -0400

    Init

---
+++ browserify.js
@@ -0,0 +1,2 @@
+ALLEX.execSuite.libRegistry.register('allex_userrepresentationlib',require('./index')(ALLEX));
+ALLEX.WEB_COMPONENTS.allex_userrepresentationlib = ALLEX.execSuite.libRegistry.get('allex_userrepresentationlib');
